8f12440ae8a2d3c7e6eac4e5f298440d709beb5c
Ilia Lisov <ilya.lisov.22@gmail.com>
2023-05-15 23:43:02 +0400

    form validation

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -3,14 +3,17 @@ import styles from './styles.module.css'
 
 class Input extends React.Component {
     render() {
-        const {name, id, type, label, placeholder, rows, value} = this.props
+        const {name, id, type, label, placeholder, rows, value, onChange, onClick, state} = this.props
+
         return (
             <div className={styles.wrapper}>
                 {type !== 'submit' && type !== 'reset' && <label htmlFor={id}>{label}</label>}
                 {rows
-                ?<textarea name={name} id={id} placeholder={placeholder} rows={rows}></textarea>
-                :<input type={type} id={id} name={name} placeholder={placeholder} value={value}/>
+                ?<textarea name={name} id={id} placeholder={placeholder} rows={rows} value={value || state?.inputs?.[name] || ''} onChange={onChange}></textarea>
+                :<input type={type} id={id} name={name} placeholder={placeholder} value={value || state?.inputs?.[name] || ''} onChange={onChange} onClick={onClick}/>
                 }
+                {state?.messages?.[name] && <span className={styles.message}>{state?.messages?.[name]}</span>}
+                {state?.errors?.[name] && <span className={styles.error}>{state?.errors?.[name]}</span>}
             </div>
         )
     }
